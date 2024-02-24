fce6f03fbcf3a1661ecc746746f1cf9feba46c1b
ruwindya <dilshanwimalasiri94@gmail.com>
2024-02-25 03:29:17 +0530

    show listing Api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Listing  from "../models/listing.model.js"
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs'
 
@@ -41,5 +42,19 @@ export const deleteUser = async (req, res , next) => {
     }catch(error){
         next(error);
     }
+}
 
+export const getUserListings = async (req, res , next) => {
+    if(req.user.id === req.params.id){
+        try{
+            const listings = await Listing.find( { userId: req.params.id });
+            res.status(200).json(listings)
+
+        }catch(error){
+            next(error)
+        }
+    }else{
+        return next(errorHandler(401, "You can view only your own listings"));
+    }
+    
 }
