6316be0a9e8de70be1f8ce2d0d09eb451aee4877
ruwindya <dilshanwimalasiri94@gmail.com>
2024-02-24 23:09:46 +0530

    bug fix

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -45,8 +45,8 @@ export const google = async (req , res ,next) =>{
     try{
         const user = await User.findOne({email: req.body.email})
         if(user) {
-            const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET)
-            const {password:pass , ...rest} = validUser._doc;
+            const token = jwt.sign({id: user._id},process.env.JWT_SECRET)
+            const {password:pass , ...rest} = user._doc;
             res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
         }else{
             const generatePassword = Math.random().toString(36).slice(-8)+Math.random().toString(36).slice(-8);
